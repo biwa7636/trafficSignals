92398d0d5e99a6b0e648ba912bdc7f903b262d79
biwa7636 <binwang.cu@gmail.com>
2015-01-31 18:35:53 -0700

    change

--- d3.js
+++ d3.js
@@ -1,11 +1,24 @@
-var path = d3.geo.path();
+var w=1000;
+var h=1000;
+
+var projection = d3.geo.albersUsa()
+                       .translate([11000, 2400])
+                       .scale([96000]);
+
+var path = d3.geo.path()
+                 .projection(projection);
+
+var svg = d3.select("#container")
+		.append("svg")
+		.attr("width", w)
+		.attr("height", h)
+		.style('background-color', 'green');
 
 d3.json("traffic_signals.json", function(json) {
-
         svg.selectAll("path")
            .data(json.features)
            .enter()
            .append("path")
            .attr("d", path);
-
 });
+
